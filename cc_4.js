92519304a7300013123a4dd97d487857c2a99637
mallikanicole <malmichael23@gmail.com>
2025-02-01 02:02:01 -0500

    Task 1-Customer Discounts

--- cc_4.js
+++ cc_4.js
@@ -1,8 +1,12 @@
-let purchaseValue=400
+//Task 1-If Statements
+let purchaseValue=400;//state purchase value
+console.log("Purchase Value",purchaseValue)
 if (purchaseValue>100) {
     console.log("Discount:",purchaseValue*.1)
 } else {
     console.log("No Discount")
-}
-const finalAmount=purchaseValue-(purchaseValue*.1)
+};//state whether discount or not
+const finalAmount=purchaseValue-(purchaseValue*.1);//calculate discounted price
 console.log("Final Amount After Discount:",finalAmount)
+
+
