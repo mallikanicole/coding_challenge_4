231adac9433023bd8f55598360723a1e7b99b84e
mallikanicole <malmichael23@gmail.com>
2025-02-01 01:52:02 -0500

    test t1

--- cc_4.js
+++ cc_4.js
@@ -1,1 +1,8 @@
 let purchaseValue=400
+if (purchaseValue>100) {
+    console.log("Discount:",purchaseValue*.1)
+} else {
+    console.log("No Discount")
+}
+const finalAmount=purchaseValue-(purchaseValue*.1)
+console.log("Final Amount After Discount:",finalAmount)
